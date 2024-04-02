b5c70dd9955f3ab0aee969361dbb3d868b0b4474
Ariane Mailloux <maia1602@usherbrooke.ca>
2024-04-02 10:57:49 -0400

    Continue targets

--- acoustique_oiseaux/_targets.r
+++ acoustique_oiseaux/_targets.r
@@ -3,7 +3,19 @@ tar_option_set(packages = c("rmarkdown", "dplyr", "RSQLite"))
 
 list(
   tar_target(
-    fichiers,
-    list.files(pattern = "\\.csv$")
+    name = fichiers,
+    command = list.files(pattern = "\\.csv$")
+  ),
+  tar_target(
+    name = dat,
+    command = lapply(fichiers, read.csv)
+  ),
+  tar_target(
+    name = donnees_combinees,
+    command = do.call(rbind, dat)
+  ),
+  tar_target(
+    name = pas_NA,
+    command = NULL_NA
   )
 )
