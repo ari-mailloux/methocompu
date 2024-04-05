5422681404c2f53e095a416d7decec4a0fc0b4d6
Ariane Mailloux <maia1602@usherbrooke.ca>
2024-04-05 10:57:54 -0400

    Targets pour les fonctions "convertir date" et "presence" pour le booleen

--- acoustique_oiseaux/_targets.r
+++ acoustique_oiseaux/_targets.r
@@ -2,6 +2,8 @@ library(targets)
 tar_option_set(packages = c("rmarkdown", "dplyr", "RSQLite"))
 
 source("null_vers_na.R")
+source("convertir_date.R")
+source("bool.R")
 
 list(
   tar_target(
@@ -19,6 +21,14 @@ list(
   tar_target(
     null_en_na,
     null_vers_na(donnees_combin)
+  ),
+  tar_target(
+    date,
+    sapply(null_en_na$date_obs, convertir_date)
+  ),
+  tar_target(
+    booleen_variable,
+    sapply(null_en_na$variable, presence)
   )
 )
 
