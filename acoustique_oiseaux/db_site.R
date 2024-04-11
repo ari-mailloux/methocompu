ff163d27ee637fb2ba4736762f1b25e9605a12c9
claras <surc1401@usherbrooke.ca>
2024-04-11 11:17:14 -0400

    Supprimer les anciens scripte de création de db

--- acoustique_oiseaux/db_site.R
+++
@@ -1,8 +0,0 @@
-creer_site <- 
-  "CREATE TABLE site (
-    site_id     INTEGER,
-    lat         VARCHAR(20),
-    projection  VARCHAR(200),
-    PRIMARY KEY (site_id)
-  );"
-dbSendQuery(conn, creer_site )
