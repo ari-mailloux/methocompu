0a90af7c9285a966dc4a38d52bc624c59f9c0437
florencebeaudry <164022336+florencebeaudry@users.noreply.github.com>
2024-04-11 11:58:16 -0400

    correction site_lat

--- acoustique_oiseaux/site_lat.R
+++ acoustique_oiseaux/site_lat.R
@@ -2,3 +2,4 @@ verifier_latitude <- function(df, colonne_lat, colonne_site_id) {
   resultats <- aggregate(df[[colonne_lat]] ~ df[[colonne_site_id]], df, function(x) length(unique(x)) == 1)
   return(resultats)
 }
+resultats_latitude <- verifier_latitude(df, "lat", "site_id")
