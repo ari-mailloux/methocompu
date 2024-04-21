5f91a50dfa7b86306ecaa4915d27e3fd2b934182
jasmineboulanger <bouj1220@usherbrooke.ca>
2024-04-21 11:33:14 -0400

    troisième graphique avec les heures en continu

--- acoustique_oiseaux/graph_creation.R
+++ acoustique_oiseaux/graph_creation.R
@@ -45,15 +45,20 @@ print(obs_par_cana_heure)
 
 
 # Création du graphique d'espèces par heure
-div_non_na <- diversite_heure[!is.na(diversite_heure$heure_formattee), ]
-
-div_par_heure <- barplot(div_non_na$nb_valid_scientific_names, 
-                         names.arg = div_non_na$heure_formattee, 
+div_non_na <- diversite[!is.na(diversite$heure_formattee), ]
+div_non_na$heure_formattee <- as.POSIXct(div_non_na$heure_formattee, format = "%H:%M:%S")
+heures_completees_div <- data.frame(heure_formattee = as.POSIXct(sprintf("%02d:00:00", 0:23), format = "%H:%M:%S"))
+donnees_completees_div <- merge(heures_completees_div, div_non_na, by = "heure_formattee", all.x = TRUE)
+donnees_completees_div$nb_valid_scientific_names[is.na(donnees_completees_div$nb_valid_scientific_names)] <- 0
+div_par_heure <- barplot(donnees_completees_div$nb_valid_scientific_names, 
+                         names.arg = format(donnees_completees_div$heure_formattee, "%H:%M:%S"), 
                          ylab = "Nombre d'espèces distinctes", 
                          xlab = "Heure", 
                          col = "skyblue",
                          ylim = c(0, 150),
                          las = 2,
                          cex.names = 0.6,
-                         main= "Nombre d'espèces\nen fonction de l'heure")
+                         main = "Nombre d'espèces distinctes\nen fonction de l'heure")
+
+# Afficher le graphique
 print(div_par_heure)
